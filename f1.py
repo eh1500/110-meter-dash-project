288afe9a1dff2cb29c185e3290608c2626c63206
jacob <95627849+eh1500@users.noreply.github.com>
2021-12-08 13:34:21 -0400

    ugrade1 f2.py

--- f1.py
+++ f1.py
@@ -19,4 +19,24 @@ for i in range(6):
 
 # message with the final score
 
+#jacovs coode
+#110 meters TYp3 be@t L000000L DG
+
+import random
+
+score = 0
+dice = [0, 0, 0, 0, 0]
+
+NAME = input("hello user plz enter ur first name"()):
+    print("welcome NAME, to 110 meter herdles!)
+    
+for i in range(6):
+    for d in range(5):
+        dice[d] = random.randint(1,6)
+        score = sum(dice)
+    print(dice)
+    question = input("do u wanna roll again? y/n")
+        if question = "n":
+            break 
+
     
