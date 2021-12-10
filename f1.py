363a197fe0d8f7e66498999466a4e206789c16eb
jacob <95627849+eh1500@users.noreply.github.com>
2021-12-10 10:17:07 -0400

    dec_fri_10_2021.py

--- f1.py
+++ f1.py
@@ -1,26 +1,4 @@
-# 110 Metre Hurdles
-# Throw all five dice, up to six times, until you are satisfied with the results
-# Scoring = total value of all five dice
-
-import random
-
-score = 0 # record's the user's score
-dice = [0, 0, 0, 0, 0]
-
-for i in range(6):
-    # roll five dice
-    for d in range(5): #0, 1, 2, 3, 4
-        dice[d] = random.randint(1,6)
-    # list dice with five random numbers
-    score = sum(dice)
-    question = input("Do you want to roll again?  y/n")
-    if question == "n":
-        break  # exit the for-loop
-
-# message with the final score
-
-#jacovs coode
-#110 meters TYp3 be@t L000000L DG
+#110 meters TYp3 be@t l000000l DG
 
 import random
 
@@ -37,4 +15,5 @@ for i in range(6):
     score = sum(dice)
     question = input("do u wanna roll again? y/n")
         if question = "n":
-            break 
+            break
+         if question = "y"
